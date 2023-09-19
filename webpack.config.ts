5259dd683c758b58feb6393dea5ad0f70df850bf
Знай Артемий <artem.znay@mail.ru>
2023-09-19 20:54:31 +0300

    add translations in prod

--- webpack.config.ts
+++ webpack.config.ts
@@ -10,7 +10,7 @@ export default (env: BuildEnv) => {
         html: path.resolve(__dirname, 'public', 'index.html'),
         src: path.resolve(__dirname, 'src'),
         locales: path.resolve(__dirname, 'public', 'locales'),
-        buildLocales: path.resolve(__dirname, 'config','build', 'locales'),
+        buildLocales: path.resolve(__dirname, 'build', 'locales'),
     };
 
     const mode = env.mode || 'development';
