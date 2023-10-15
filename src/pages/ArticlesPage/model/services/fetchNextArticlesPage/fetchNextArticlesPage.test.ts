bbfa97ca7d894a9e053da339f3222e533ff970ae
Знай Артемий <artem.znay@mail.ru>
2023-10-15 14:07:16 +0300

    add jest-html-reporters

--- src/pages/ArticlesPage/model/services/fetchNextArticlesPage/fetchNextArticlesPage.test.ts
+++ src/pages/ArticlesPage/model/services/fetchNextArticlesPage/fetchNextArticlesPage.test.ts
@@ -1,8 +1,8 @@
-import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk';
-import { fetchNextArticlesPage } from './fetchNextArticlesPage';
-import { fetchArticlesList } from '../fetchArticlesList/fetchArticlesList';
+import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk'
+import { fetchNextArticlesPage } from './fetchNextArticlesPage'
+import { fetchArticlesList } from '../fetchArticlesList/fetchArticlesList'
 
-jest.mock('../fetchArticlesList/fetchArticlesList');
+jest.mock('../fetchArticlesList/fetchArticlesList')
 
 describe('fetchNextArticlesPage.test', () => {
     test('success', async () => {
@@ -13,15 +13,15 @@ describe('fetchNextArticlesPage.test', () => {
                 entities: {},
                 limit: 5,
                 isLoading: false,
-                hasMore: true,
-            },
-        });
+                hasMore: true
+            }
+        })
 
-        await thunk.callThunk();
+        await thunk.callThunk()
 
-        expect(thunk.dispatch).toBeCalledTimes(4);
-        expect(fetchArticlesList).toHaveBeenCalledWith({ page: 3 });
-    });
+        expect(thunk.dispatch).toBeCalledTimes(4)
+        expect(fetchArticlesList).toHaveBeenCalledWith({})
+    })
     test('fetchAritcleList not called', async () => {
         const thunk = new TestAsyncThunk(fetchNextArticlesPage, {
             articlesPage: {
@@ -30,13 +30,13 @@ describe('fetchNextArticlesPage.test', () => {
                 entities: {},
                 limit: 5,
                 isLoading: false,
-                hasMore: false,
-            },
-        });
+                hasMore: false
+            }
+        })
 
-        await thunk.callThunk();
+        await thunk.callThunk()
 
-        expect(thunk.dispatch).toBeCalledTimes(2);
-        expect(fetchArticlesList).not.toHaveBeenCalled();
-    });
-});
+        expect(thunk.dispatch).toBeCalledTimes(2)
+        expect(fetchArticlesList).not.toHaveBeenCalled()
+    })
+})
